fd0311e99931d19ee48ebff4721f901d31658e6f
jorgealmir <jorgealmirmartins@gmail.com>
2020-11-07 00:15:58 -0200

    Retirada do código para API 3.0

--- src/Payments.php
+++ src/Payments.php
@@ -25,10 +25,6 @@ class Payments extends Message
     private $params;
     private $callBack;
     private $curlError;
-
-
-    private $environment;
-    private $merchant;
     
     /**
      * Inicia a classe com o ambiente selecionado
@@ -52,159 +48,10 @@ class Payments extends Message
             "MerchantId: {$this->merchantId}",
             "MerchantKey: {$this->merchantKey}"
         ];
-        
-//        try {
-//            if ($sandbox) {
-//                $this->environment = Environment::sandbox();
-//            } else {
-//                $this->environment = Environment::production();
-//            }
-//        
-//            $this->merchant = new Merchant($merchantId, $merchantKey);
-//        } catch (CieloRequestException $e) {
-//            $this->setError($e->getCieloError()->getCode());
-//        }
     }
-    
-//    public function tokenizingCard(array $data) 
-//    { 
-//        $card = new CreditCard();
-//        $card->setCustomerName($data['customerName']);
-//        $card->setCardNumber($data['cardNumber']);
-//        $card->setHolder($data['holder']);
-//        $card->setExpirationDate($data['expirationDate']);
-//        $card->setSecurityCode($data['securityCode']);
-//        $card->setBrand($data['brand']);
-//        // CardToken: 495db52c-17c8-4698-af28-98bdff5b5339
-//
-//        try {
-//            $respCard = (new CieloEcommerce($this->merchant, $this->environment))->tokenizeCard($card);
-//            
-//            $cardToken = $respCard->getCardToken();
-//        
-//            $this->setMessage(21, "", "", $cardToken);
-//        } catch (CieloRequestException $e) {
-//            $this->setError($e->getCieloError()->getCode());
-//        }
-//    }
-    
-//    public function payCreditCardTokenized(array $data) 
-//    {
-//        $sale = new Sale($data['order']);
-//
-//        $sale->customer($data['customerName']);
-//
-//        $payment = $sale->payment($data['amount']);
-//        
-//        $payment->setCapture(1);
-//
-//        $payment->setType(Payment::PAYMENTTYPE_CREDITCARD)
-//            ->creditCard($data['securityCode'], $data['brand'])
-//            ->setCardToken($data['cardToken']);
-//
-//        try {
-//            $responseSale = (new CieloEcommerce($this->merchant, $this->environment))->createSale($sale);
-//            
-//            $this->setMessage(
-//                $responseSale->getPayment()->getStatus(),
-//                $responseSale->getPayment()->getReturnCode()
-//            );
-//            
-//            $this->setTid($responseSale->getPayment()->getTid());
-//            $this->setPaymentId($responseSale->getPayment()->getPaymentId());
-//            
-//            var_dump($responseSale);
-//        } catch (CieloRequestException $e) {
-//            $this->setError($e->getCieloError()->getCode());
-//        }
-//    }
-    
-    /**
-     * Pagamento com Cartão de crédito
-     * @param array $data
-     */
-//    public function payCreditCard(array $data) 
-//    {
-//        $sale = new Sale($data['order']);
-//        
-//        $sale->customer($data['customerName']);
-//        
-//        $payment = $sale->payment($data['amount']);
-//        
-//        $payment->setCapture(1);
-//        
-//        $payment->setType(Payment::PAYMENTTYPE_CREDITCARD)
-//            ->creditCard($data['securityCode'], $data['brand'])
-//            ->setExpirationDate($data['expirationDate'])
-//            ->setCardNumber($data['cardNumber'])
-//            ->setHolder($data['holder'])
-//            ->setSaveCard(true);
-//        
-//        try {
-//            $retSale = (new CieloEcommerce($this->merchant, $this->environment))->createSale($sale);
-//           
-//            $token = $retSale->getPayment()->getCreditCard()->getCardToken();
-//            var_dump($token);
-//            
-//            $this->setMessage(
-//                $retSale->getPayment()->getStatus(),
-//                $retSale->getPayment()->getReturnCode()
-//            );
-//            
-//            $this->setTid($retSale->getPayment()->getTid());
-//            $this->setPaymentId($retSale->getPayment()->getPaymentId());
-//            
-//            var_dump($retSale);
-//        } catch (CieloRequestException $e) {
-//            $err = $e->getCieloError()->getCode();
-//            $this->setError($err);
-//        }
-//    }
-    
-    /**
-     * Pay Recurrent
-     * @param array $data
-     * @return type
-     */
-//    public function payRecurrent(array $data) {
-//        $sale = new Sale($data['MerchantOrderId']);
-//
-//        $sale->customer($data['Customer']['Name']);
-//
-//        $payment = $sale->payment($data['Payment']['Amount']);
-//        
-//        $payment->setCapture(1);
-//
-//        $payment->setType(Payment::PAYMENTTYPE_CREDITCARD)
-//            ->creditCard($data['Payment']['CreditCard']['SecurityCode'], $data['Payment']['CreditCard']['Brand'])
-//            ->setExpirationDate($data['Payment']['CreditCard']['ExpirationDate'])
-//            ->setCardNumber($data['Payment']['CreditCard']['CardNumber'])
-//            ->setHolder($data['Payment']['CreditCard']['Holder']);
-//
-//        $payment->recurrentPayment(true)->setInterval(RecurrentPayment::INTERVAL_MONTHLY);
-//
-//        try {
-//            $paySale = (new CieloEcommerce($this->merchant, $this->environment))->createSale($sale);
-//
-////            $recurrentPaymentId = $paySale->getPayment()->getPaymentId();
-//        
-//            var_dump($paySale);
-//            
-//            $this->setMessage(
-//                $paySale->getPayment()->getStatus(),
-//                $paySale->getPayment()->getReturnCode(), 
-//                $paySale->getPayment()->getTid()
-//            );
-//        } catch (CieloRequestException $e) {
-//            $this->setError($e->getCieloError()->getCode());
-//        }
-//    }
-    
-    
-    /*
-     * Sem API
-     */
-    
+            
+
+        
     /*
      * Transações com GETs
      */
@@ -216,6 +63,8 @@ class Payments extends Message
         return $this->callBack;
     }
     
+    
+    
     public function getCreditCardData($cardNumber) 
     {
         $number = substr($cardNumber, 1, 6);
@@ -224,6 +73,8 @@ class Payments extends Message
         
         return $this->callBack;
     }
+    
+    
     
     public function queryByPaymentId($paymentId) 
     {
@@ -240,6 +91,8 @@ class Payments extends Message
         return $pay;
     }
     
+    
+    
     public function queryByOrderId($merchantOrderId) 
     {
         $this->endPoint = "/1/sales?merchantOrderId={$merchantOrderId}";
@@ -252,6 +105,8 @@ class Payments extends Message
         
         return $order;
     }
+    
+    
     
     public function queryRecurrent($recurrentPaymentId) 
     {
